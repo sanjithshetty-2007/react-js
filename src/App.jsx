6911c2ad1ee462db125dea046830ee9807585fa0
A <sanjith@gmail.com>
2026-07-15 13:09:47 +0530

    Initial vite app

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import "./App.css";
+
+const Card = () => {
+  return (
+    <div className="div">
+      <h2>functional arrow conmponenrt</h2>
+    </div>
+  );
+};
+const App = () => {
+  return (
+    <>
+      <Card />
+    </>
+  );
+};
+
+export default App;
