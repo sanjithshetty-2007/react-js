512314e0c947ea2ae31ff6cc77990cb242dfc104
A <sanjith@gmail.com>
2026-07-15 16:49:57 +0530

    modfied assets and adding search.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,16 @@ import React from "react";
 const App = () => {
   return (
     <main>
-      <div className="pattern">
-        <div className="wrapper">
-          <header>
-            <h1>
-              Find <span className="text-gradient">Movies</span> You'll enjoy
-              Without the Hassle
-            </h1>
-          </header>
-          <p>search</p>
-        </div>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="hero banner " />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll enjoy
+            Without the Hassle
+          </h1>
+        </header>
+        <p>search</p>
       </div>
     </main>
   );
